adb073c7cd1076f1fee3637dd64a2ccea5f17693
Oliver Leics <oliver.leics@gmail.com>
2012-02-09 11:23:07 +0100

    Commented out plain copying. Replaced with creation of zip-archives

--- mk.bat
+++ mk.bat
@@ -10,22 +10,34 @@ echo %PATH%
 
 if not exist "%TMP_DIR%" (
   mkdir "%TMP_DIR%"
+  if not errorlevel 0 goto error
 )
 
 if not exist "%DIST_DIR%" (
   mkdir "%DIST_DIR%"
+  if not errorlevel 0 goto error
 )
 
 for /R "packs" %%a in (*.txt) do (
   echo %%~na
-  if not exist "./dist/%%~na" (
-    mkdir "%DIST_DIR%/%%~na"
+  if exist "%DIST_DIR%/%%~na.zip" (
+    del "%DIST_DIR:/=\%\%%~na.zip"
+    if not errorlevel 0 goto error
   )
+  rem if not exist "./dist/%%~na" ( mkdir "%DIST_DIR%/%%~na" )
   for /F "tokens=1,2,3 delims=," %%b in (%%a) do (
     echo %%b %%c %%d
-    xcopy /I /E /V /Y "%VENDOR_DIR%/%%b/%%c" "%DIST_DIR%/%%~na/%%d"
+    rem xcopy /I /E /V /Y "%VENDOR_DIR%/%%b/%%c" "%DIST_DIR%/%%~na/%%d"
+    rem if not errorlevel 0 goto error
+    7za a -tzip "%DIST_DIR%/%%~na.zip" "%VENDOR_DIR%/%%b/%%c/*" -mx9
+    if not errorlevel 0 goto error
   )
 )
 
+:error
+echo ERROR
+goto end
+
+:end
 set PATH=%PREV_PATH%
 echo.
